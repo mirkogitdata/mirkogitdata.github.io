31090cd83fed75a6c1c1b4934a7c6718bc30f18c
mirkogitdata <45661184+mirkogitdata@users.noreply.github.com>
2019-07-25 08:29:53 +0200

    Update scrollTo.js

--- scrollTo.js
+++ scrollTo.js
@@ -1,35 +1,22 @@
   scrollTo(id) =>{
-		 
 		 if($(id).length) {
-			 
 			 var getOffset = $(id).offset().top;
-			 
 			 var targetDistance = 50;
 			 
 			 $('html,body').animate({
-				 
 				 srcollTop: getOffset - targetDistance
 			 }, 500);
 		 }
-		 
 	 }
 	 
 	 $('.scrollTo').click(()=> {
-		 
 		 var getElement = $(this).attr('href');
-		 
 		 if($(getElement).length) {
-			 
 			 var getOffset = $(getElement).offset().top;
-			 
 			 var targetDistance = 0;
-			 
 			 $('html,body').animate({
-				 
 				 scrollTop: getOffset - targetDistance
-				 
 			 }, 1200);
 		 }
-		 
 		 return false;
 	 });
